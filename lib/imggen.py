7749d96bff1ba3e183c006a502ff4da14ccf7c4b
gary-drocella/TangenT <gdrocella@Garys-MacBook-Pro.local>
2021-06-04 14:51:25 -0400

    Fix the license comment error

--- lib/imggen.py
+++ lib/imggen.py
@@ -1,18 +1,15 @@
-
-/**
- * stash is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * stash is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with stash.  If not, see <https://www.gnu.org/licenses/>.
- */
+# stash is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# stash is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with stash.  If not, see <https://www.gnu.org/licenses/>.
  
 from random import randint
 from pathlib import Path
@@ -22,7 +19,7 @@ class ImgGen:
     def __init__(self, fname, key, files):
         self.fname = fname
         self.files = files
-        self.key = bytes(key, 'utf-8')[0]
+        self.key = bytes(int(key))[0]
 
     def generate(self):
         findex = 0
