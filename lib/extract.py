bdd54dba80fb6198e60a4faacc84a49b5f27fbab
gary-drocella/TangenT <gdrocella@Garys-MacBook-Pro.local>
2021-06-04 23:31:02 -0400

    Add license

--- lib/extract.py
+++ lib/extract.py
@@ -1,3 +1,16 @@
+# stash is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# stash is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with stash.  If not, see <https://www.gnu.org/licenses/>.
+
 from lib import magic
 from pathlib import Path
 from lib.file import File
